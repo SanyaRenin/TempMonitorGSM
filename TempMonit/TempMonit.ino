cf07d4fb543847dd4d41e6133711dbfeb361da81
Alexandr Belonogov <sanya_renin@mail.ru>
2018-07-28 12:58:01 +0500

    battery level and timer function
    - добавлена функции таймера и определения заряда батареи

--- TempMonit/TempMonit.ino
+++ TempMonit/TempMonit.ino
@@ -1,19 +1,26 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include "DHT.h"
+#include <DS3231.h>
 
-#define AnalogRead 2000 //период чтения напряжения, ms
-#define DrawTime 200 //период обновления экрана, ms
+#define AnalogRead 1000 //период проверки обновлений значений температуры, ms
+#define DrawTime 2000 //период обновления экрана, ms
 #define KeyReadTime 20 //период проверки нажатия клавиатуры
 
 #define ONE_WIRE_BUS 2 // Data wire is plugged into port 2 on the Arduino
 #define DHTPIN 3     // what digital pin we're connected to
+#define BattaryPin A3 //пин, принимающий сигнал от батареи
+
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define MaxBatteryVoltage 4.2 //Напряжение полностью заряженной батареи (значение на ацп)
+#define MinBatteryVoltage 2.9 //Напряжение полностью разряженной батареи (значение на ацп)
+#define t1Hour 20 //час отправки значений, ms
+#define t1Minute 10 //минута отправки значений, ms
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DHT dht(DHTPIN, DHTTYPE);
-
+DS3231 Clock;
 DeviceAddress Thermometer1 = { 
   0x28, 0xFF, 0x31, 0x67, 0x63, 0x16, 0x03, 0xF2 };  // адрес датчика DS18B20 
 DeviceAddress Thermometer2 = { 
@@ -24,9 +31,76 @@ bool dt = false; //флаг обновления экрана
 bool rt = false; //флаг чтения температуры
 bool kr = 0; //флаг периода проверки клавиатуры
 
+//clock variable
+byte year, month, date, doW, hour, minute, second;
+bool Century=false;
+bool h12;
+bool PM, g;
 
+bool timerCompleted = 0;
+float temp1, temp2, temp3, humidity;
+
+//---------------- ФУНКЦИИ ----------------
+
+//функция возвратит 1 только при первом совпадении времени таймера и текущего времени
+bool checkTimer() {
+  if (hour == t1Hour && minute == t1Minute) {
+    if (!timerCompleted) {
+      timerCompleted = 1;
+      return 1;  
+    } else {
+      return 0; 
+    }
+  } else {
+    timerCompleted = 0;
+    return 0; 
+  }
+}
+
+byte getBatteryLevel() {
+  byte batLevelPercent = analogRead(BattaryPin)/(MaxBatteryVoltage/5*1024)*100;
+  if (batLevelPercent >= 75) return 4;
+  if (batLevelPercent >= 50) return 3;
+  if (batLevelPercent >= 25) return 2;
+  if (batLevelPercent > 5)  return 1;
+  if (batLevelPercent >= 0) return 0;
+}
+
+void getTime() {
+  year=Clock.getYear();
+  month=Clock.getMonth(Century);
+  date=Clock.getDate();
+  hour=Clock.getHour(h12,PM); 
+  minute=Clock.getMinute();  
+  second=Clock.getSecond();
+}
+
+void updateTempValue() {
+  sensors.requestTemperatures();
+  temp1 = sensors.getTempC(Thermometer1);
+  temp2 = sensors.getTempC(Thermometer2);
+  temp3 = dht.readHumidity();
+  humidity = dht.readTemperature();
+  
+  Serial.print("temp1 = ");
+  Serial.println(temp1);
+  Serial.print("temp2 = ");
+  Serial.println(temp2);
+  Serial.print("Humidity = ");
+  Serial.println(humidity);
+  Serial.print("temp3 = ");
+  Serial.println(temp3);
+  Serial.println();  
+}
+
+//---------------- System ФУНКЦИИ ----------------
 void DrawMenu () {
- 
+  getTime();
+  updateTempValue();
+  if (checkTimer()) {
+    
+  }
+  
 }
 
 void KeyPad () {
@@ -34,27 +108,10 @@ void KeyPad () {
 }
 
 void ReadAnalog () {
-  // put your main code here, to run repeatedly:
-  sensors.requestTemperatures();
-  float  tempC = sensors.getTempC(Thermometer1);
-  float  tempB = sensors.getTempC(Thermometer2);
-  Serial.print("temp1 = ");
-  Serial.println(tempC);
-  Serial.print("temp2 = ");
-  Serial.println(tempB);
   
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  Serial.print("Humidity = ");
-  Serial.println(h);
-  Serial.print("temp3 = ");
-  Serial.println(t);
-  Serial.println();  
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   OCR0A = 0xAF; //прерывание
   TIMSK0 |= _BV(OCIE0A); //прерывание
@@ -65,15 +122,15 @@ void setup() {
   dht.begin();
 }
 
-SIGNAL(TIMER0_COMPA_vect) { //прерывание считывающие мощность
+//прерывание, формирует мини операционную систему (system)
+SIGNAL(TIMER0_COMPA_vect) { 
   tyme = tyme + 1;
   if (tyme % AnalogRead == 0) { rt = 1; };
   if (tyme % DrawTime == 0) { dt = 1; };
   if (tyme % KeyReadTime == 0) { kr = 1; };
 }
 
-void loop()
-{
+void loop() {
   if ((rt == 1)) { rt = 0; ReadAnalog();}; //в режиме архива измерений не происходит 
   if (dt == 1) { dt = 0; DrawMenu(); };
   if (kr == 1) { kr = 0; KeyPad(); }; 
